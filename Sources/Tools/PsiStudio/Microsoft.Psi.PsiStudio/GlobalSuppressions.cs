54814b1305246efd0f75168a880a55c21503571e
John Elliott <John.Elliott@microsoft.com>
2018-03-17 23:53:11 +0000

    Merged PR 15716: fixed doxygen output; added back xml doc gen to PsiStudio
    fixed doxygen output; added back xml doc gen to PsiStudio

--- Sources/Tools/PsiStudio/Microsoft.Psi.PsiStudio/GlobalSuppressions.cs
+++
@@ -1,4 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Reviewed.")]
