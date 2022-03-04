14d72a14f5ecbe6cc6c80b5cf215b691b60009cd
Nick Saw <chitsaw@microsoft.com>
2022-03-03 17:36:06 -0800

    Fixed context menu items and margins in text visualizers

--- Sources/Visualization/Microsoft.Psi.Visualization.Windows/VisualizationObjects/TextVisualizationObject.cs
+++ Sources/Visualization/Microsoft.Psi.Visualization.Windows/VisualizationObjects/TextVisualizationObject.cs
@@ -3,6 +3,7 @@
 
 namespace Microsoft.Psi.Visualization.VisualizationObjects
 {
+    using System.ComponentModel;
     using System.Runtime.Serialization;
     using System.Windows;
     using Microsoft.Psi.Visualization.Helpers;
@@ -16,8 +17,22 @@ namespace Microsoft.Psi.Visualization.VisualizationObjects
     [VisualizationPanelType(VisualizationPanelType.Canvas)]
     public class TextVisualizationObject : StreamValueVisualizationObject<string>
     {
+        private Thickness margin = new (5, 0, 0, 0);
+
         /// <inheritdoc />
         [IgnoreDataMember]
         public override DataTemplate DefaultViewTemplate => XamlHelper.CreateTemplate(this.GetType(), typeof(TextVisualizationObjectView));
+
+        /// <summary>
+        /// Gets or sets the margin.
+        /// </summary>
+        [DataMember]
+        [DisplayName("Margin")]
+        [Description("The left, top, right and bottom margin in pixels.")]
+        public Thickness Margin
+        {
+            get { return this.margin; }
+            set { this.Set(nameof(this.Margin), ref this.margin, value); }
+        }
     }
 }
