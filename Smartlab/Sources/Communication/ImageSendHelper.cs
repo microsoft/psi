4bbd648a1a9bae517a54448a867ba12e9e0c70e7
rcmurray <>
2023-02-02 19:23:40 -0500

    Merging tweaks

--- Smartlab/Sources/Communication/ImageSendHelper.cs
+++ Smartlab/Sources/Communication/ImageSendHelper.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CMU.Smartlab.Communication
+// namespace Smartlab.Communication
+// namespace Communication
 {
     public class ImageSendHelper
     {
